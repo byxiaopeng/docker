01c0623c2022c8457c131e4fdf8fd2dc718b4c83
小朋 <2810580330@qq.com>
2020-10-24 16:11:00 +0800

    Update entrypoint.sh

--- ibm-docker/entrypoint.sh
+++ ibm-docker/entrypoint.sh
@@ -33,6 +33,9 @@ EOF
 # start nginx
 nginx
 # Run V2Ray
-/etc/init.d/gov2 start
-ss-server -s 127.0.0.1 -p 9090  -k peng  -m chacha20-ietf-poly1305 -t 300 -d 8.8.8.8 -u --plugin v2ray-plugin --plugin-opts "server;path=/peng;loglevel=none"
+#/etc/init.d/gov2 start
+/usr/bin/v2ray -config /usr/local/etc/v2ray/config.json >/dev/null 2>&1 &
+gost -L ws://:7070?path=/gows >/dev/null 2>&1 &
+ss-server -s 127.0.0.1 -p 9090  -k peng  -m chacha20-ietf-poly1305 -t 300 -d 8.8.8.8 -u --plugin v2ray-plugin --plugin-opts "server;path=/peng;loglevel=none" >/dev/null 2>&1 &
+brook wsserver -l 127.0.0.1:6060 --path /brook -p peng >/dev/null 2>&1 &
 tail -f /dev/null
